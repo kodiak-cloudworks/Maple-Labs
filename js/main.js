83e47bbd4e1d4cabcbae9a306f2744c43f25900e
Jagan <jagannath.kulkarni@maplelabs@.com>
2018-05-23 10:39:06 +0530

    Scroll to the specific container is fixed

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ jQuery(function($) {
 	});
 
 	$('.navbar-collapse ul li a').on('click', function() {  
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 70}, 1000);
 		return false;
 	});
 
